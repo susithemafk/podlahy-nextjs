ed60c81de8ab1f2849260d327fc80dcd9bb4c208
susithemafk <100321407+susithemafk@users.noreply.github.com>
2022-05-10 09:50:01 +0200

    Update button-small.tsx

--- components/buttons/button-small.tsx
+++ components/buttons/button-small.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
 
-const ButtonSmall: NextPage<Props> = () => {
+const ButtonSmall: NextPage = () => {
   return (
     <div className={styles.container}>
         <nav>
